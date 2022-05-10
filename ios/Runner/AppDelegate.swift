cc178e39e3b7e12a7020c03867316f86cc3507ee
ChrisElliotUK <christopher.elliot6@gmail.com>
2022-05-10 15:41:04 +0100

    init

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,32 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func initFlutterChannel() {
+        if let controller = window?.rootViewController as? FlutterViewController {
+            let channel = FlutterMethodChannel(
+                name: "com.example.com.watch",
+                binaryMessenger: controller.binaryMessenger
+            )
+            channel.setMethodCallHandler({[weak self] (
+                call: FlutterMethodCall,
+                result: @escaping FlutterResult) -> Void in
+                switch call.method {
+                case "flutterToWatch":
+                    guard let watchSession = self?.session, watchSession.isPaired,
+                          watchSession.isReachable, let methodData = call.arguments as [String: Any],
+                          let method = methodData["method"], let data = methodData["data"] as? Any else {
+                        result(false)
+                        return
+                    }
+                    
+                    let watchData: [String: Any] = ["method": method, "data": data]
+                    watchSession.sendMessage(watchData, replyHandler: nil, errorHandler: nil)
+                    result(true)
+                    
+                default: result(FlutterMethodNotImplemented)
+                }
+            })
+        }
+    }
 }
